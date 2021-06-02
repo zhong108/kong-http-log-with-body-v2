07ba6c74896dbf7e0df4449b499565d5dcaaff5d
Boyan Zlatanov <b.zlatanov@tinqin.com>
2021-06-02 22:45:55 +0300

    Add debug logs

--- kong/plugins/kong-http-log-with-body-v2/handler.lua
+++ kong/plugins/kong-http-log-with-body-v2/handler.lua
@@ -166,6 +166,10 @@ function HttpLogHandler:log(conf)
     -- body may be empty
     local request_status, req_body = pcall(parse_body, kong.request.get_header("Content-Type"), ctx.request_body)
     local response_status, res_body = pcall(parse_body, kong.response.get_header("Content-Type"), ctx.response_body)
+    kong.log.err("request_status: ", request_status)
+    kong.log.err("req_body: ", req_body)
+    kong.log.err("request_status: ", request_status)
+    kong.log.err("res_body:", res_body)
     if request_status then
         log_obj.request.body = req_body
     else
