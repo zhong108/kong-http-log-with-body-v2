60322b9f7ba4d243e2de661dd21b6ddff7b8449f
Boyan Zlatanov <b.zlatanov@tinqin.com>
2021-06-09 23:21:02 +0300

    Use raw body if not JSON

--- kong/plugins/kong-http-log-with-body-v2/handler.lua
+++ kong/plugins/kong-http-log-with-body-v2/handler.lua
@@ -159,7 +159,9 @@ function HttpLogHandler:body_filter(conf)
     else
         if not eof then
             local json_text = '["RawData": "' .. chunk .. '"]'
+            local json_encoded = { RawData = chunk }
             kong.log("json_text: ", json_text)
+            kong.log("json_encoded: ", json_encoded)
             ctx.response_body = cjson_decode(json_text)
         end
     end
